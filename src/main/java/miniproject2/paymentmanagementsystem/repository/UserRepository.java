50447a1b77a898f74470248f33e9a7f87944ef66
akhilesh <vanaparthia@zeta.tech>
2025-08-14 13:58:57 +0530

    added user repository

---
+++ src/main/java/miniproject2/paymentmanagementsystem/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package miniproject2.paymentmanagementsystem.repository;
+
+import miniproject2.paymentmanagementsystem.entity.User;
+import miniproject2.paymentmanagementsystem.enums.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    List<User> findByRole(Role role);
+    boolean existsByEmail(String email);
+}
